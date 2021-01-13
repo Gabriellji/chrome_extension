44d9ddbea21fd2c01671b2e0996e85174b6cde10
Gabriellji <gabriellji@yahoo.com>
2021-01-14 00:51:06 +0300

    feat: besic extention

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,17 @@
-// This file is injected as a content script
-console.log("Hello from content script!")
+import "./content.css";
+
+const body = document.getElementsByTagName("body");
+
+const heartContainer = document.createElement("div");
+heartContainer.className = "snowflakes";
+heartContainer.setAttribute("aria-hidden", "true");
+
+const heart = document.createElement("div");
+heart.className = "snowflake";
+heart.innerHTML = "<3";
+
+for (let i = 0; i < 12; i++) {
+    heartContainer.appendChild(heart.cloneNode(true));
+}
+
+body[0]?.prepend(heartContainer);
